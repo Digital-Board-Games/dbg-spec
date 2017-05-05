2aac855d91921edbebbd16f2f43ba529536ce72b
wrgoldstein <williamrgoldstein@gmail.com>
2017-05-04 23:57:10 -0400

    More specific file naming

--- spec/slide_movement_spec.rb
+++ spec/slide_movement_spec.rb
@@ -33,29 +33,18 @@ describe Piece do
   context 'a basic piece' do
     let(:movement) do
       {
-        move: [
-          [1,0],
-          [0,1],
-          [-1,0],
-          [0,-1],
-          [1,1],
-          [-1,-1],
-          [-1,1],
-          [1,-1]
-        ],
-        jump: [],
         slide: [
           [[1,1], [2,2], [3,3], [4,4]],
           [[-1,1], [-2,2], [-3,3], [-4,4]],
-        ],
-        attack: []
+        ]
       }
     end
     let(:piece) { Piece.new(movement) }
-    it 'can move anywhere on an empty board' do
+    let(:all_moves) { movement[:slide].flatten(1) }
+    it 'can slide anywhere on an empty board' do
       gb = Gameboard.new
       moves = piece.slide_moves(gb)
-      expect(moves).to eq movement[:slide].flatten(1)
+      expect(moves).to eq(all_moves)
     end
     it "can't slide where blocked" do
       # With a piece on (1,1) of our gameboard,
@@ -63,7 +52,6 @@ describe Piece do
       # are blocked.
       gb = Gameboard.new
       gb[1][1] = 1
-      all_moves = movement[:slide].flatten(1)
       moves = piece.slide_moves(gb)
       expect(moves).to eq(all_moves - [[1,1], [2,2], [3,3], [4,4]])
     end
